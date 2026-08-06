81b82f41574f409855629662433878cab1e0ced9
mysterystories3008-beep <mysterystories3008@gmail.com>
2026-08-06 05:06:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static(path.join(__dirname, "/frontend")));
 
 const DAILY_SCORE_FILE = path.join(__dirname, "daily-scores.json");
 const WEEKLY_SCORE_FILE = path.join(__dirname, "weekly-scores.json");
