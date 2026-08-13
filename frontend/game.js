6c934abf02599068e1da33c30a3884f38961eaf6
mysterystories3008-beep <mysterystories3008@gmail.com>
2026-08-13 02:41:50 +0200

    Update game.js

--- frontend/game.js
+++ frontend/game.js
@@ -11,7 +11,7 @@ async function updateLiveStatus() {
     try {
 const backendUrl = window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://satoshiplays.com";
+    : "http://satoshiplays.com";
 
         const response = await fetch(`${backendUrl}/api/status`);
 
@@ -624,7 +624,7 @@ this.bestText = this.add.text(30, 75, "Best: " + highScore, {
 if (!socket) {
     const backendUrl = window.location.hostname === "localhost"
         ? "http://localhost:3000"
-        : "https://api.satoshiplays.com";
+        : "http://satoshiplays.com";
 
    socket = io(backendUrl);
 
