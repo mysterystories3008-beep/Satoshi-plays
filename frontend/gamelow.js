c56274cba4395f6899273a6899d37f655d3c0729
mysterystories3008-beep <mysterystories3008@gmail.com>
2026-08-13 04:40:30 +0200

    Update gamelow.js

--- frontend/gamelow.js
+++ frontend/gamelow.js
@@ -193,7 +193,7 @@ class GameScene extends Phaser.Scene {
     const backendUrl =
     window.location.hostname === "localhost"
         ? "http://localhost:3000"
-        : "https://satoshi-plays.onrender.com";
+        : "https://api.satoshiplays.com";
 
 socket = io(backendUrl);
 
