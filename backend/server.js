db4dfcb785b1a606be925b05fa8f01f5c1ab2a8f
mysterystories3008-beep <mysterystories3008@gmail.com>
2026-08-15 05:16:01 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -506,6 +506,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
